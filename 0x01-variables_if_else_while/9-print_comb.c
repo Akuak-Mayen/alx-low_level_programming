2aa8727f556dbaa3f4f66b950ffde7187ceec7af
Akuak-Mayen <mayenakuak84@gmail.com>
2023-06-18 14:31:55 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,27 +3,23 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints all possible combinations of two-digit numbers,
+ * Description: Prints all possible combinations of single-digit numbers,
  *              separated by ", " and in ascending order.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num1, num2;
+	int num;
 
-	for (num1 = 0; num1 <= 8; num1++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (num2 = num1 + 1; num2 <= 9; num2++)
-		{
-			putchar(num1 + '0');
-			putchar(num2 + '0');
+		putchar(num + '0');
 
-			if (num1 != 8 || num2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
