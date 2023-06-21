a1ad0ca436fb6bcec47bf82124dbff2af1e27a14
Akuak-Mayen <mayenakuak84@gmail.com>
2023-06-21 14:12:27 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -26,4 +26,24 @@
 			i++;
 		}
 	}
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ */
+void print_alphabet_x10(void)
+{
+    /* Function implementation */
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
 
