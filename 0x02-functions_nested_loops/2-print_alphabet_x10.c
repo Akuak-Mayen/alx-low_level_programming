eed3fda8219017053c96013e21e0b8a5760e2e76
Akuak-Mayen <mayenakuak84@gmail.com>
2023-06-20 18:19:47 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -22,3 +22,29 @@ void print_alphabet_x10(void)
 	_putchar('\n');
 }
 
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: On success 1, on error -1
+ */
+int _putchar(char c)
+{
+	if (write(1, &c, 1) == -1)
+		return (-1);
+	return (1);
+}
+
