ea43236f3c0423ca8c03ee16f065a68ac462d111
Akuak-Mayen <mayenakuak84@gmail.com>
2023-06-21 13:43:32 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,50 +1,29 @@
 #include "main.h"
 
-/**
- * print_alphabet_x10 - Prints the alphabet in lowercase ten times
- */
-void print_alphabet_x10(void)
-{
-	int count = 0;
-	char letter;
 
-	while (count < 10)
+	/**
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
+	 */
+	void print_alphabet_x10(void)
 	{
-		letter = 'a';
-		while (letter <= 'z')
+		char ch;
+		int i;
+
+
+		i = 0;
+
+
+		while (i < 10)
 		{
-			_putchar(letter);
-			letter++;
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+			_putchar('\n');
+			i++;
 		}
-		count++;
 	}
-
-	_putchar('\n');
-}
-
-#include "main.h"
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-/**
- * _putchar - Writes a character to stdout
- * @c: The character to be written
- *
- * Return: On success 1, on error -1
- */
-int _putchar(char c)
-{
-	if (write(1, &c, 1) == -1)
-		return (-1);
-	return (1);
-}
 
