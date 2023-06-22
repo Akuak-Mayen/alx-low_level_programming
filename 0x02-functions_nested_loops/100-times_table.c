cc2b0b819060dedd6639357bb1e9cc78df717c1b
Akuak-Mayen <mayenakuak84@gmail.com>
2023-06-21 17:48:56 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_number - prints a number with appropriate formatting
- * @num: the number to be printed
+ * print_number - Prints a number with appropriate formatting.
+ * @num: The number to be printed.
  */
 void print_number(int num)
 {
 	if (num < 10)
 	{
+		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(' ');
@@ -15,6 +16,7 @@ void print_number(int num)
 	}
 	else if (num >= 10 && num < 100)
 	{
+		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 		_putchar((num / 10) + '0');
@@ -22,6 +24,7 @@ void print_number(int num)
 	}
 	else if (num >= 100)
 	{
+		_putchar(',');
 		_putchar(' ');
 		_putchar((num / 100) + '0');
 		_putchar(((num / 10) % 10) + '0');
@@ -30,31 +33,40 @@ void print_number(int num)
 }
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print_times_table_row - Prints a single row of the times table.
+ * @n: The number for the times table.
+ * @row: The row number to print.
+ */
+void print_times_table_row(int n, int row)
+{
+	int i, result;
+
+	for (i = 0; i <= n; i++)
+	{
+		result = i * row;
+
+		if (i == 0)
+			_putchar(result + '0');
+		else
+			print_number(result);
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: Number of the times table.
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				int result = j * i;
-
-				if (j == 0)
-					_putchar(result + '0');
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					print_number(result);
-				}
-			}
-			_putchar('\n');
+			print_times_table_row(n, i);
 		}
 	}
 }
